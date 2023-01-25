4238e772cfd07f2f77bf9344c26d886003ccb051
vaibhavb479 <vaibhavb479@gmail.com>
2023-01-25 18:14:55 +0530

    Angular-Demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild  } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { AgGridAngular } from 'ag-grid-angular';
+import { CellClickedEvent, ColDef, GridReadyEvent } from 'ag-grid-community';
+import { Observable } from 'rxjs';
+import { AgCheckbox } from 'ag-grid';
+
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +14,43 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'agGrid';
+  shouldRun = /(^|.)(stackblitz|webcontainer).(io|com)$/.test(window.location.host);
+  
+  // Each Column Definition results in one Column.
+  public columnDefs: ColDef[] = [
+  { field: 'make', checkboxSelection:true},
+  { field: 'model'},
+  { field: 'price' }
+];
+
+ // DefaultColDef sets props common to all Columns
+ public defaultColDef: ColDef = {
+  sortable: true,
+  filter: true,
+};
+
+ // Data that gets displayed in the grid
+ public rowData$!: Observable<any[]>;
+
+ // For accessing the Grid's API
+ @ViewChild(AgGridAngular) agGrid!: AgGridAngular;
+
+ constructor(private http: HttpClient) {}
+
+ // Example load data from sever
+ onGridReady(params: GridReadyEvent) {
+   this.rowData$ = this.http
+     .get<any[]>('https://www.ag-grid.com/example-assets/row-data.json');
+ }
+
+ // Example of consuming Grid Event
+ onCellClicked( e: CellClickedEvent): void {
+   console.log('cellClicked', e);
+ }
+
+ // Example using Grid's API
+ clearSelection(): void {
+   this.agGrid.api.deselectAll();
+ }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,43 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { AgGridModule } from 'ag-grid-angular';
+import { HttpClientModule } from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatTableModule} from '@angular/material/table';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
+import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatStepperModule} from '@angular/material/stepper';
+import {MatSidenavModule} from '@angular/material/sidenav';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ContactComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    AgGridModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule,
+    MatTableModule,
+    MatProgressSpinnerModule,
+    MatMenuModule,
+    MatStepperModule,
+    MatSidenavModule
   ],
   providers: [],
   bootstrap: [AppComponent]
